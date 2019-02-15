93af47515f16e1851c4e408420a9590305ba5cfe
restlessmedia <phillycooper@hotmail.com>
2019-02-15 14:46:44 +0000

    Create IClauseCollectionExtensionsTests.cs

---
+++ SqlBuilder.UnitTest/IClauseCollectionExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SqlBuilder.UnitTest
+{
+  [TestClass]
+  public class IClauseCollectionExtensionsTests
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+      //A.Fake<IClauseCollection<T>>
+      ///IClauseCollectionExtensions.
+    }
+
+    private class TestClass
+    {
+
+    }
+  }
+}
